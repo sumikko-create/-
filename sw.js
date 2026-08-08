6a630c5abb402c00bf2c4ab3df1a895031d3760f
sumikko-create <ptrk0503@gmail.com>
2026-08-08 09:35:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+/* オフラインで動かすための最小サービスワーカー。
+   index.html を編集したら CACHE の日付を変える（そうしないと古いのが出る）。 */
+const CACHE = "dive-2026-08-07";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
